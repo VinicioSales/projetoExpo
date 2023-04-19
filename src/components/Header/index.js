7b0b15c5037b1bfc7a09f53c3be8861eb24b0190
VinicioSales <vinicio.santtana@gmail.com>
2023-04-19 18:02:11 -0300

    teste

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,37 @@
-import React from "react"
+//import React from "react"
 import {
     View,
     StyleSheet,
     Text,
     StatusBar,
-    TouchableOpacity
+    TouchableOpacity,
+    TextInput
 } from 'react-native'
 import {Feather} from '@expo/vector-icons'
+import React, {useState} from 'react'
 
 const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 64;
 
 export default function Header(){
+    const [inputValue, setInputValue] = useState('');
     return(
-        <View style={styles.container}>
+        /*<View style={styles.container}>
             <View style={styles.content}>
                 <Text style={styles.username}>Sujeito Prog</Text>
                 <TouchableOpacity activeOpacity={0.9} style={styles.buttonUser}>
                     <Feather name='user' size={27} color='#fff'/>
                 </TouchableOpacity>
             </View>
+        </View>*/
+        <View>
+            <TextInput
+                placeholder='Digite aqui'
+                value={inputValue}
+                onChangeText={setInputValue}
+            />
         </View>
-    )
+        
+    );
 }
 
 const styles = StyleSheet.create(
